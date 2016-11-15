e8fae9886d951d549fbf7af5f55d09857e6cb10b
Igor <igorvich1988@hotmail.com>
2016-11-15 16:09:20 +0100

    Added IdentityElement class

---
+++ app/models/IdentityElement.java
@@ -0,0 +1,29 @@
+package models;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+
+/**
+ * Created by Igor on 11/15/2016.
+ */
+@Entity
+public class IdentityElement {
+
+    @Id
+    public long id;
+    public String identityElementText;
+
+    public IdentityElement()
+    {}
+
+    public static Model.Finder<Integer, IdentityElement> find = new Model.Finder<>(IdentityElement.class);
+
+    public static List<IdentityElement> getAllOccupations()
+    {
+        List<IdentityElement> identityElements = Ebean.find(IdentityElement.class).findList();
+        return identityElements;
+    }
+}
