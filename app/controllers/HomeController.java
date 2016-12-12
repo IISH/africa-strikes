bdb1651655c817e0ae2336e8be5d3a6847195b1d
Igor <igorvich1988@hotmail.com>
2016-12-12 11:30:41 +0100

    Updated the application
    HomeController:
    Added try catch around the code to save the strike.
    Checks if the strike is valid and returns error messages to display on the page.
    admin.scala:
    Adjusted the way the page is shown
    Added a logout button to the page
    index.scala:
    Changed message to multiple messages for the errors
    Added Strike variable
    Added logout button
    Changed the way info is displayed in the selectize elements, this for when a Strike is inserted incorrectly and redisplayed.
    Edited the Add Strike button
    update.scala:
    Edited the buttons

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -62,10 +62,15 @@ public class HomeController extends Controller{
             saveYamlFileToDatabase((List<CompanyName>) Yaml.load("company-name-data.yml"));
         }
 
+        Map<String, String> messages = new HashMap<>();
+        return handleRequest(messages, new Strike());
+    }
+
+    private Result handleRequest(Map<String, String> errorMessages, Strike strike) {
         List<Integer> years = IntStream.rangeClosed(1700, 1950).boxed().collect(Collectors.toList());
         List<Integer> days = IntStream.rangeClosed(0, 31).boxed().collect(Collectors.toList());
-        return ok(index.render("",
-                formFactory.form(Strike.class),
+        return ok(index.render(errorMessages,
+                formFactory.form(Strike.class).fill(strike),
                 Sector.find.all(),
                 (List<String>) Yaml.load("source-data.yml"),
                 OccupationHisco.find.all(),
@@ -77,7 +82,8 @@ public class HomeController extends Controller{
                 (List<String>) Yaml.load("months.yml"),
                 (List<String>) Yaml.load("number-of-participants.yml"),
                 years,
-                days));
+                days,
+                strike));
     }
 
     private <T> void saveYamlFileToDatabase(List<T> yamlList)
@@ -92,105 +98,95 @@ public class HomeController extends Controller{
         }
     }
 
-    private Html handleBadForm(String message)
-    {
-        List<Integer> years = IntStream.rangeClosed(1700, 1950).boxed().collect(Collectors.toList());
-        List<Integer> days = IntStream.rangeClosed(0, 31).boxed().collect(Collectors.toList());
-        return (index.render(message,
-                formFactory.form(Strike.class),
-                Sector.find.all(),
-                (List<String>) Yaml.load("source-data.yml"),
-                OccupationHisco.find.all(),
-                CauseOfDispute.find.all(),
-                IdentityElement.find.all(),
-                StrikeDefinition.find.all(),
-                (List<String>) Yaml.load("country-data.yml"),
-                (List<String>) Yaml.load("labour-relations.yml"),
-                (List<String>) Yaml.load("months.yml"),
-                (List<String>) Yaml.load("number-of-participants.yml"),
-                years,
-                days));
-    }
-
     public Result addStrike()
     {
-        Http.MultipartFormData body = request().body().asMultipartFormData();
-        Strike strike = formFactory.form(Strike.class).bindFromRequest().get();
-
-        // Code to save the given article image
-        Http.MultipartFormData.FilePart<File> article = body.getFile("articleUpload");
-        if(article.getFilename().length() > 0) {
-            String[] temp = article.getFilename().split("\\.");
-            String extension = temp[temp.length - 1].toString();
-
-            // creating the file with the correct path
-            File articleFile = null;
-            try {
-                articleFile = File.createTempFile("article-", "." + extension, new File("C:\\AfricaStrikes\\Articles\\"));
-            } catch (Exception e) {
-            }
-
-            // Checks if the file has an image extension
-            if (Pattern.compile("((?i)(jpg|png|gif|bmp)$)").matcher(extension).matches()) {
-                BufferedImage image = null;
-                try {
-                    image = ImageIO.read(article.getFile());
-                    ImageIO.write(image, extension, articleFile);
-                } catch (Exception e) {
-                    e.getMessage();
-                }
-            }
-            // Checks if the file has a pdf or tif extension
-            else if (Pattern.compile("((?i)(pdf|tiff|tif)$)").matcher(extension).matches()) {
-                try {
-                    FileInputStream fs = new FileInputStream(article.getFile());
-                    int b;
-                    FileOutputStream os = new FileOutputStream(articleFile);
-                    while ((b = fs.read()) != -1) {
-                        os.write(b);
-                    }
-                    os.close();
-                    fs.close();
-                } catch (Exception E) {
-                    E.printStackTrace();
-                }
-            }
-
-            strike.setArticle(new Article(articleFile.getName()));
-        }
-        strike.setChecked(false);
-        strike.setAuthorInformation(ctx().session().get("username"));
-
         Ebean.beginTransaction();
         try {
-            // --------------------------------------------------------------------------------- \\
-            // Maps the sectors given by the form and puts them in the sectors list of the Strike
-            Map<?, Sector> sectorMap = Sector.find.findMap();
-            strike.setSectors(mapSelectedOptionsToTheStrike(body, strike, "sectors[]", sectorMap));
+            Http.MultipartFormData body = request().body().asMultipartFormData();
+            String[] postAction = request().body().asMultipartFormData().asFormUrlEncoded().get("indexButton");
+            if("logout".equals(postAction[0])){
+                securityController.logout();
+            }else if("add".equals(postAction[0])) {
+                Strike strike = formFactory.form(Strike.class).bindFromRequest().get();
 
-            // --------------------------------------------------------------------------------- \\
-            // Maps the occupations given by the form and puts them in the occupations list of the Strike
-            Map<?, OccupationHisco> occupationHiscoMap = OccupationHisco.find.findMap();
-            strike.setHiscoOccupations(mapSelectedOptionsToTheStrike(body, strike, "hiscoOccupations[]", occupationHiscoMap));
+                // Code to save the given article image
+                Http.MultipartFormData.FilePart<File> article = body.getFile("articleUpload");
+                if (article.getFilename().length() > 0) {
+                    String[] temp = article.getFilename().split("\\.");
+                    String extension = temp[temp.length - 1].toString();
 
-            // --------------------------------------------------------------------------------- \\
-            // Maps the causeOfDisputes given by the form and puts them in the causeOfDisputes list of the Strike
-            Map<?, CauseOfDispute> causeOfDisputeMap = CauseOfDispute.find.findMap();
-            strike.setCauseOfDisputes(mapSelectedOptionsToTheStrike(body, strike, "causeOfDisputes[]", causeOfDisputeMap));
+                    // creating the file with the correct path
+                    File articleFile = null;
+                    try {
+                        articleFile = File.createTempFile("article-", "." + extension, new File("C:\\AfricaStrikes\\Articles\\"));
+                    } catch (Exception e) {
+                    }
 
-            // --------------------------------------------------------------------------------- \\
-            // Maps the identityElements given by the form and puts them in the identityElements list of the Strike
-            Map<?, IdentityElement> identityElementMap = IdentityElement.find.findMap();
-            strike.setIdentityElements(mapSelectedOptionsToTheStrike(body, strike, "identityElements[]", identityElementMap));
+                    // Checks if the file has an image extension
+                    if (Pattern.compile("((?i)(jpg|png|gif|bmp)$)").matcher(extension).matches()) {
+                        BufferedImage image = null;
+                        try {
+                            image = ImageIO.read(article.getFile());
+                            ImageIO.write(image, extension, articleFile);
+                        } catch (Exception e) {
+                            e.getMessage();
+                        }
+                    }
+                    // Checks if the file has a pdf or tif extension
+                    else if (Pattern.compile("((?i)(pdf|tiff|tif)$)").matcher(extension).matches()) {
+                        try {
+                            FileInputStream fs = new FileInputStream(article.getFile());
+                            int b;
+                            FileOutputStream os = new FileOutputStream(articleFile);
+                            while ((b = fs.read()) != -1) {
+                                os.write(b);
+                            }
+                            os.close();
+                            fs.close();
+                        } catch (Exception E) {
+                            E.printStackTrace();
+                        }
+                    }
 
-            // --------------------------------------------------------------------------------- \\
-            // Maps the strikeDefinitions given by the form and puts them in the strikeDefinitions list of the Strike
-            Map<?, StrikeDefinition> strikeDefinitionMap = StrikeDefinition.find.findMap();
-            strike.setStrikeDefinitions(mapSelectedOptionsToTheStrike(body, strike, "strikeDefinitions[]", strikeDefinitionMap));
+                    strike.setArticle(new Article(articleFile.getName()));
+                }
+                strike.setChecked(false);
+                strike.setAuthorInformation(ctx().session().get("username"));
 
-            // Saves the strike
-            strike.save();
-            Ebean.commitTransaction();
+                // --------------------------------------------------------------------------------- \\
+                // Maps the sectors given by the form and puts them in the sectors list of the Strike
+                Map<?, Sector> sectorMap = Sector.find.findMap();
+                strike.setSectors(mapSelectedOptionsToTheStrike(body, strike, "sectors[]", sectorMap));
+
+                // --------------------------------------------------------------------------------- \\
+                // Maps the occupations given by the form and puts them in the occupations list of the Strike
+                Map<?, OccupationHisco> occupationHiscoMap = OccupationHisco.find.findMap();
+                strike.setHiscoOccupations(mapSelectedOptionsToTheStrike(body, strike, "hiscoOccupations[]", occupationHiscoMap));
+
+                // --------------------------------------------------------------------------------- \\
+                // Maps the causeOfDisputes given by the form and puts them in the causeOfDisputes list of the Strike
+                Map<?, CauseOfDispute> causeOfDisputeMap = CauseOfDispute.find.findMap();
+                strike.setCauseOfDisputes(mapSelectedOptionsToTheStrike(body, strike, "causeOfDisputes[]", causeOfDisputeMap));
+
+                // --------------------------------------------------------------------------------- \\
+                // Maps the identityElements given by the form and puts them in the identityElements list of the Strike
+                Map<?, IdentityElement> identityElementMap = IdentityElement.find.findMap();
+                strike.setIdentityElements(mapSelectedOptionsToTheStrike(body, strike, "identityElements[]", identityElementMap));
+
+                // --------------------------------------------------------------------------------- \\
+                // Maps the strikeDefinitions given by the form and puts them in the strikeDefinitions list of the Strike
+                Map<?, StrikeDefinition> strikeDefinitionMap = StrikeDefinition.find.findMap();
+                strike.setStrikeDefinitions(mapSelectedOptionsToTheStrike(body, strike, "strikeDefinitions[]", strikeDefinitionMap));
+
+                System.out.println("Value of valid is: " + checkIfValid(strike));
+                if (checkIfValid(strike).size() != 0) {
+                    return handleRequest(checkIfValid(strike), strike);
+                }
+
+                // Saves the strike
+                strike.save();
+                Ebean.commitTransaction();
+            }
         }
         catch (NullPointerException e)
         {
@@ -201,6 +197,30 @@ public class HomeController extends Controller{
             Ebean.endTransaction();
         }
         return redirect(routes.HomeController.index());
+    }
+
+    private Map<String, String> checkIfValid(Strike strike)
+    {
+        Map<String, String> result = new HashMap<>();
+        if (strike.getYearStart() == 0) {
+            result.put("yearStart", "You need to select the year the strike started!!");
+        }
+        if (strike.getSource().isEmpty()) {
+            result.put("source", "You need to fill in a source!!");
+        }
+        if (strike.getCountry().isEmpty()) {
+            result.put("country", "You need to select a country!!");
+        }
+        if (strike.getWorkersSituation() == null) {
+            result.put("workersSituation", "You need to select what the workers on strike are!!");
+        }
+        if (strike.getDominantGender() == null) {
+            result.put("dominantGender", "You need to select a predominant gender!!");
+        }
+        if (strike.getOccupations().isEmpty()) {
+            result.put("occupations", "You need to fill in at least one occupation!!");
+        }
+        return result;
     }
 
     public Result getStrikes(){
