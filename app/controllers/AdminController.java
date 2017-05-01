3c7be961215b11f6cf1aff26d84987d7235a22db
Igor <igorvich1988@hotmail.com>
2017-05-01 16:10:44 +0200

    Adjusted the way the author information was checked, due to conversion
    The previous version gave an array, now it gets a string.

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -166,8 +166,8 @@ public class AdminController extends Controller{
         } else {
             if ("save".equals(postAction[0])) {
                 if(!strikeSelected.getChecked()) { // check if the strike has already been approved
-                    Http.MultipartFormData body = request().body().asMultipartFormData();
-                    if (body.asFormUrlEncoded().get("authorInformation").equals(ctx().session().get("username"))) { // check if author provided and user are the same
+                    if (formFactory.form(Strike.class).bindFromRequest().get().getAuthorInformation().equals(ctx().session().get("username"))) { // check if author provided and user are the same
+                        Http.MultipartFormData body = request().body().asMultipartFormData();
                         updateStrike(body);
                     }else {return badRequest(noStrikeSelected.render("You cannot update the author information of the strike!","/admin"));}
                 }else {return badRequest(noStrikeSelected.render("You cannot update the selected strike!","/admin"));}
